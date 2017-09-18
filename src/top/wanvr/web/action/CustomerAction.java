89167b0c03eab172ad05a005508a74be617d8486
aongi <aongi@github.com>
2017-09-18 21:28:11 +0800

    新增客户添加与文件上传功能

--- src/top/wanvr/web/action/CustomerAction.java
+++ src/top/wanvr/web/action/CustomerAction.java
@@ -1,14 +1,18 @@
 package top.wanvr.web.action;
 
+
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 import com.opensymphony.xwork2.ModelDriven;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.struts2.ServletActionContext;
 import org.hibernate.criterion.DetachedCriteria;
 import org.hibernate.criterion.Restrictions;
 import top.wanvr.entity.Customer;
 import top.wanvr.service.CustomerService;
 import top.wanvr.utils.PageBean;
+
+import java.io.File;
 
 public class CustomerAction extends ActionSupport implements ModelDriven<Customer> {
 
@@ -18,6 +22,38 @@ public class CustomerAction extends ActionSupport implements ModelDriven<Custome
 
     private Integer currentPage;
     private Integer pageSize;
+
+
+    //接收上传的图片
+    private File photo;
+
+    //接收上传的图片名称(在提交的键名后加上FileName即可)
+    private String photoFileName;
+    private String photoContextType;//文件MIME类型
+
+    public String getPhotoContextType() {
+        return photoContextType;
+    }
+
+    public void setPhotoContextType(String photoContextType) {
+        this.photoContextType = photoContextType;
+    }
+
+    public String getPhotoFileName() {
+        return photoFileName;
+    }
+
+    public void setPhotoFileName(String photoFileName) {
+        this.photoFileName = photoFileName;
+    }
+
+    public File getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(File photo) {
+        this.photo = photo;
+    }
 
     public CustomerService getCs() {
         return cs;
@@ -58,7 +94,17 @@ public class CustomerAction extends ActionSupport implements ModelDriven<Custome
         ActionContext.getContext().put("pageBean", pb);
         return "list";
     }
+    //添加客户
+    public String add(){
+        //文件上传
+        photo.renameTo(new File(ServletActionContext.getRequest().getContextPath()+"/upload/"+photoFileName));
+        //---------------------------------
+        //调用service，保存Customer对象
+        cs.save(customer);
+        //重定向到Action
+        return "toList";
 
+    }
     @Override
     public Customer getModel() {
         return customer;
